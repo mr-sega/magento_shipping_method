a84880be7591f1425ff672bd9511bf59e76f9b33
Spicy Tea <mr.segamega11@gmail.com>
2022-10-17 00:34:24 +0300

    add attribute

--- app/code/Sega/ProductHeight/Block/Height.php
+++ app/code/Sega/ProductHeight/Block/Height.php
@@ -2,24 +2,53 @@
 
 namespace Sega\ProductHeight\Block;
 
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Registry;
+use Magento\Catalog\Model\Product;
 use Magento\Framework\View\Element\Template;
 
 class Height extends Template
 {
 
-    protected $_registry;
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @var Product
+     */
+    protected $product;
 
     public function __construct(
         Template\Context $context,
-        Registry $registry
-    ){
-        $this->_registry = $registry;
-        parent::__construct($context);
+        Registry $registry,
+        array $data)
+    {
+        $this->registry = $registry;
+
+        parent::__construct($context, $data);
     }
 
-    public function getCurrentProduct()
+    /**
+     * @return Product
+     */
+    public function getProduct()
     {
-        return $this->_registry->registry('current_product');
+        if (is_null($this->product)) {
+            $this->product = $this->registry->registry('product');
+
+            if (!$this->product->getId()) {
+                throw new LocalizedException(__('Failed to initialize product'));
+            }
+        }
+
+        return $this->product;
+    }
+
+
+    public function getProductHeight()
+    {
+        return $this->getProduct()->getData('product_height');
     }
 }
